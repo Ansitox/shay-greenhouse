05a12717aa18281d89e9acc543ba31e0451f8464
Adrián Da Silva <adriandasilva23@hotmail.com>
2024-08-14 19:31:43 -0300

    tab icon changed. routes config done.

---
+++ src/components/Ah/Ah.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Ah = () => {
+  return <div>Ah</div>;
+};
---
+++ src/routes/routes.js
@@ -0,0 +1,9 @@
+import { Ah } from "../components/Ah/Ah";
+
+export const routes = [
+  {
+    id: 1,
+    path: "/",
+    Element: Ah,
+  },
+];
